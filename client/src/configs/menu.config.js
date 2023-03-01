2c4ff4d2ed9f92895bb2604a2bc33554568e0e13
Pranay Dhongade <77525888+PranayD1807@users.noreply.github.com>
2023-03-01 15:16:32 +0530

    Configs Added

---
+++ client/src/configs/menu.config.js
@@ -0,0 +1,62 @@
+import HomeOutlineIcon from "@mui/icons-material/HomeOutlined";
+import SlideShowOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import RateReviewOutlinedIcon from "@mui/icons-material/RateReviewOutlined";
+import LockResetOutlinedIcon from "@mui/icons-material/LockResetOutlined";
+
+const main = [
+  {
+    display: "home",
+    path: "/",
+    icon: <HomeOutlineIcon />,
+    state: "home",
+  },
+  {
+    display: "movie",
+    path: "/movie",
+    icon: <SlideShowOutlinedIcon />,
+    state: "movie",
+  },
+  {
+    display: "tv series",
+    path: "/tv",
+    icon: <LiveTvOutlinedIcon />,
+    state: "homtve",
+  },
+  {
+    display: "search",
+    path: "/search",
+    icon: <SearchOutlinedIcon />,
+    state: "search",
+  },
+];
+
+const user = [
+  {
+    display: "favorite",
+    path: "/favorite",
+    icon: <FavoriteBorderOutlinedIcon />,
+    state: "favorite",
+  },
+  {
+    display: "reviews",
+    path: "/reviews",
+    icon: <RateReviewOutlinedIcon />,
+    state: "reviews",
+  },
+  {
+    display: "password update",
+    path: "/password-update",
+    icon: <LockResetOutlinedIcon />,
+    state: "password.update",
+  },
+];
+
+const menuConfigs = {
+  main,
+  user,
+};
+
+export default menuConfigs;
