437cd48981ff0230346a038f4e12b2d94d78aef6
Pranay Dhongade <77525888+PranayD1807@users.noreply.github.com>
2023-03-02 15:06:39 +0530

    Global Loading Added

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import GlobalLoading from "../common/GlobalLoading";
 const MainLayout = () => {
   return (
     <>
       {/* Global loading */}
+      <GlobalLoading />
       {/* Global loading */}
 
       {/* login modal */}
