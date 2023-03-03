46bdf3b5573ca2f5ba88ebfbd95a8f91199c5867
Pranay Dhongade <77525888+PranayD1807@users.noreply.github.com>
2023-03-03 22:47:49 +0530

    Auth Modal

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import AuthModal from "../common/AuthModal";
 import Footer from "../common/Footer";
 import GlobalLoading from "../common/GlobalLoading";
 import Topbar from "../common/Topbar";
@@ -11,6 +12,7 @@ const MainLayout = () => {
       {/* Global loading */}
 
       {/* login modal */}
+      <AuthModal />
       {/* login modal */}
 
       <Box display="flex" minHeight="100vh">
