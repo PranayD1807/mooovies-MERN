0ef50b4bc868bc792d9260887b2306c184813830
Pranay Dhongade <77525888+PranayD1807@users.noreply.github.com>
2023-03-04 10:59:52 +0530

    User & JWT

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -4,7 +4,40 @@ import AuthModal from "../common/AuthModal";
 import Footer from "../common/Footer";
 import GlobalLoading from "../common/GlobalLoading";
 import Topbar from "../common/Topbar";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+import userApi from "../../api/modules/user.api";
+import favoriteApi from "../../api/modules/favorite.api";
+import { setListFavorites, setUser } from "../../redux/features/userSlice";
 const MainLayout = () => {
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const authUser = async () => {
+      const { response, err } = await userApi.getInfo();
+
+      if (response) dispatch(setUser(response));
+      if (err) dispatch(setUser(null));
+    };
+
+    authUser();
+  }, [dispatch]);
+
+  useEffect(() => {
+    const getFavorites = async () => {
+      const { response, err } = await favoriteApi.getList();
+
+      if (response) dispatch(setListFavorites(response));
+      if (err) toast.error(err.message);
+    };
+
+    if (user) getFavorites();
+    if (!user) dispatch(setListFavorites([]));
+  }, [user, dispatch]);
+
   return (
     <>
       {/* Global loading */}
