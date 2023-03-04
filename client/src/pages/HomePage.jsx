9cd1aa258ff246007eca28984f22be7c96fd5fab
Pranay Dhongade <77525888+PranayD1807@users.noreply.github.com>
2023-03-04 15:04:37 +0530

    Movie Slide Item & Slider

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import HeroSlide from "../components/common/HeroSlide";
 import tmdbConfigs from "../api/configs/tmdb.configs";
-
+import { Box } from "@mui/material";
+import uiConfigs from "../configs/ui.configs";
+import Container from "../components/common/Container";
+import MediaSlide from "../components/common/MediaSlide";
 const HomePage = () => {
   return (
     <>
@@ -9,6 +12,32 @@ const HomePage = () => {
         mediaType={tmdbConfigs.mediaType.movie}
         mediaCategory={tmdbConfigs.mediaCategory.popular}
       />
+      <Box marginTop="-4rem" sx={{ ...uiConfigs.style.mainContent }}>
+        <Container header="Popular Movies">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.movie}
+            mediaCategory={tmdbConfigs.mediaCategory.popular}
+          />
+        </Container>
+        <Container header="Popular Series">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.tv}
+            mediaCategory={tmdbConfigs.mediaCategory.popular}
+          />
+        </Container>
+        <Container header="Top Rated Movies">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.movie}
+            mediaCategory={tmdbConfigs.mediaCategory.top_rated}
+          />
+        </Container>
+        <Container header="Top Rated Series">
+          <MediaSlide
+            mediaType={tmdbConfigs.mediaType.tv}
+            mediaCategory={tmdbConfigs.mediaCategory.top_rated}
+          />
+        </Container>
+      </Box>
     </>
   );
 };
